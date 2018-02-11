dc6f66bd8ca2dbe52c900b85096912ef2433ed29
TGS <tgs@tgs.blue>
2018-02-11 20:09:33 +0800

    finish get_gender_from_name

--- src/utility.ts
+++ src/utility.ts
@@ -1,9 +1,9 @@
-import baidu_baike_link from 'baidu-baike-link'
-import { Option } from 'funfix-core'
 import Gender from '@enum-all/gender'
-import baidu_baike_parser from 'baidu-baike-parser'
-import axios from 'axios'
 import { get_gender } from '@wiki-analyzer/gender'
+import axios from 'axios'
+import baidu_baike_link from 'baidu-baike-link'
+import baidu_baike_parser from 'baidu-baike-parser'
+import { Option } from 'funfix-core'
 import * as _ from 'lodash'
 
 async function get_html(url: string): Promise<string> {
@@ -23,11 +23,9 @@ export async function get_gender_from_name(name: string): Promise<Option<Gender>
     const link_gender__list = await Promise.all(links.map(get_gender_from_link))
     const gender__list = link_gender__list.filter(lg => lg.gender__opt.nonEmpty()).map(lg => lg.gender__opt.get())
     if (_.isEmpty(gender__list)) {
-        console.log('Empty list')
         return Option.none()
     }
-    console.log(gender__list)
-    if (_.every(gender__list, (a => a == Gender.male))) return Option.of(Gender.male)
-    if (_.every(gender__list, (a => a == Gender.female))) return Option.of(Gender.female)
+    if (_.every(gender__list, (a => a === Gender.male))) return Option.of(Gender.male)
+    if (_.every(gender__list, (a => a === Gender.female))) return Option.of(Gender.female)
     return Option.none()
 }
