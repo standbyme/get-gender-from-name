bf195cd5ab306538268d5b1339d48a68b2e82891
TGS <tgs@tgs.blue>
2018-02-11 20:23:59 +0800

    Get Gender from Name

--- src/utility.spec.ts
+++ src/utility.spec.ts
@@ -1,22 +1,29 @@
-import * as assert from 'assert'
-import { get_gender_from_name } from './utility'
 import Gender from '@enum-all/gender'
+import * as assert from 'assert'
 
-describe('Abstract Parser', function () {
+import { get_gender_from_name } from './utility'
+
+describe('Get Gender from Name', function () {
     this.slow(5000)
-    this.timeout(10000)
+    this.timeout(100000)
 
     it('Return Some if it OK', async function () {
         const result = await get_gender_from_name('李克强')
         assert.equal(result.get(), Gender.male)
     })
 
+    // it('Return Some if it OK', async function () {
+    //     const result = await get_gender_from_name('王毅')
+    //     assert.equal(result.get(), Gender.male)
+    // })
+
     it('Return Some if it OK', async function () {
         const result = await get_gender_from_name('董明珠')
         assert.equal(result.get(), Gender.female)
     })
-    // it('Return None if it fails', async function () {
-    //     const result = await get_gender_from_name('李克强')
-    //     assert.equal(result.get(), Gender.male)
-    // })
+
+    it('Return None if it fails', async function () {
+        const result = await get_gender_from_name('刘涛')
+        assert(result.isEmpty())
+    })
 })
