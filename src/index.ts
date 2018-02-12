4e3ca2b12030c2e25b814deb505ecf6da97b962b
TGS <tgs@tgs.blue>
2018-02-12 14:57:32 +0800

    add koa

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,34 @@
 import Gender from '@enum-all/gender'
+import * as fs from 'fs'
 import * as _ from 'lodash'
 import { who } from 'xtranslation'
 
 import { get_gender_from_name } from './utility'
-
-const str = `
-中国国务院侨办副主任谭天星12日在中国驻悉尼总领事馆副总领事童学军陪同下走到访悉尼华星艺术团。
-悉尼华星艺术团团长余俊武介绍了刚刚结束的《文化中国•华星闪耀》的活动情况，
-并表示自己在活动中深深感受到了海外华人华侨对中华文化的渴望而产生着巨大的能量，
-而华星艺术团就是要把这些能量凝聚起来，在本土形成传承弘扬中华文化的强大实力。
-`
+// tslint:disable-next-line:variable-name
+const Koa = require('koa')
+const app = new Koa()
+const koaBody = require('koa-body')
+const cors = require('koa-cors')
 
 function convert(gender: Gender): string {
     if (gender === Gender.male) return '男'
     else return '女'
 }
 
-async function main() {
-    const person__list = (await who({ para: str }))
-    console.log(person__list)
-    const name__list = person__list.map(p => p.name)
-    const person_with_gender_promise__list = name__list.map(get_gender_from_name)
-    const gender__list = (await Promise.all(person_with_gender_promise__list)).map(g => g.map(convert).getOrElse('N/A'))
-    console.log(_.zip(name__list, gender__list))
+async function get_gender_from_person(person: { name: string, org: string, job: string }): Promise<{ name: string, org: string, job: string, gender: string }> {
+    const { name, org, job } = person
+    const gender = (await get_gender_from_name(name)).map(convert).getOrElse('N/A')
+    return { name, gender, org, job }
 }
 
-main()
+app.use(koaBody())
+app.use(cors())
+
+// tslint:disable-next-line:no-any
+app.use(async (ctx: any) => {
+    const person__list = (await who({ para: decodeURIComponent(ctx.request.body.UserInput) })).map(p => ({ name: p.name, org: p.org.getOrElse('N/A'), job: p.job.getOrElse('N/A') }))
+    const person_with_gender__list = await Promise.all(person__list.map(get_gender_from_person))
+    ctx.body = {person_with_gender__list}
+})
+
+app.listen(1234)
