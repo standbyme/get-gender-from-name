fceb3700c3557c205d2a8db87d334e3248de97b5
TGS <stand-by-me@mail.com>
2018-02-13 22:06:45 +0800

    add timeout retry

--- src/utility.ts
+++ src/utility.ts
@@ -6,8 +6,41 @@ import baidu_baike_parser from 'baidu-baike-parser'
 import { Option } from 'funfix-core'
 import * as _ from 'lodash'
 
+// tslint:disable-next-line:no-any
+axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err: any) {
+    const config = err.config
+    // If config does not exist or the retry option is not set, reject
+    if (!config || !config.retry) return Promise.reject(err)
+
+    // Set the variable for keeping track of the retry count
+    config.__retryCount = config.__retryCount || 0
+
+    // Check if we've maxed out the total number of retries
+    if (config.__retryCount >= config.retry) {
+        // Reject with the error
+        return Promise.reject(err)
+    }
+
+    // Increase the retry count
+    config.__retryCount += 1
+
+    // Create new promise to handle exponential backoff
+    // tslint:disable-next-line:no-any
+    const backoff = new Promise(function (resolve: any) {
+        setTimeout(function () {
+            resolve()
+        }, config.retryDelay || 1)
+    })
+
+    // Return the promise in which recalls axios to retry the request
+    return backoff.then(function () {
+        return axios(config)
+    })
+})
+
 async function get_html(url: string): Promise<string> {
-    const { data } = await axios.get(url)
+    // @ts-ignore
+    const { data } = await axios.get(url, { retry: 3, retryDelay: 3000 })
     return data
 }
 
